6e9872f85e3026cfeeb53de5e5c6180ce47f245b
pskdream <dram-1234567@hotmail.com>
2020-10-06 15:42:45 +0700

    6/10

--- Queues/ArrayListQueue.cs
+++ Queues/ArrayListQueue.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Queues
 {
-    public class Class1
+    class ArrayListQueue
     {
     }
 }
---
+++ Queues/LinkedListQueue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lists;
+
+namespace Queues
+{
+    public class LinkedListQueue : Queue
+    {
+        private List list = new LinkedList();
+        public bool isEmpty() { return list.isEmpty(); }
+        public int size() { return list.size(); }
+        public void enqueue(object e) { }
+        public object dequeue() { 
+        }
+        public object peek() { }
+    }
+
+}
