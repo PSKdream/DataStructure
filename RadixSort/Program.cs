4f34597979a1d714c72e779debc9a666ed05ae2b
pskdream <dram-1234567@hotmail.com>
2020-10-06 16:00:38 +0700

    6/10

---
+++ RadixSort/Program.cs
@@ -0,0 +1,21 @@
+﻿using Queues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadixSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                String text = "Q" + i;
+                Queue data = new ArrayQueue(10);
+            }
+        }
+    }
+}
