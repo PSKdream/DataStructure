59c44f1b7ca7e71681442e26f75350e8a91f3e1c
pskdream <dram-1234567@hotmail.com>
2020-10-13 22:11:20 +0700

    Clean Code

--- Postfix/Properties/AssemblyInfo.cs
+++ Postfix/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("main")]
+[assembly: AssemblyTitle("Postfix")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("main")]
+[assembly: AssemblyProduct("Postfix")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a568057b-64de-4298-b089-f8cf8f74a810")]
+[assembly: Guid("dcd28baa-6a1c-4f86-968c-6acb5e6863a6")]
 
 // Version information for an assembly consists of the following four values:
 //
