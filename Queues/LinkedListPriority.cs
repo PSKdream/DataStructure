626acdb722faf8bda96156b02070aa176275be51
pskdream <dram-1234567@hotmail.com>
2020-10-13 22:35:30 +0700

    Linklist priority

--- Queues/LinkedListPriority.cs
+++ Queues/LinkedListPriority.cs
@@ -13,8 +13,9 @@ namespace Queues
 
         public object dequeue()
         {
-            object e = peek();
-            list.remove(0);
+            int i = HighestPriorityIndex();
+            object e = list.get(i);
+            list.remove(i);
             return e;
         }
 
@@ -30,14 +31,26 @@ namespace Queues
 
         public object peek()
         {
-            if (isEmpty())
-                throw new System.MissingMemberException();
-            return list.get(0);
+            return list.get(HighestPriorityIndex());
         }
 
         public int size()
         {
             return list.size();
+        }
+
+        private int HighestPriorityIndex()
+        {
+            if (isEmpty())
+                throw new System.MissingMemberException();
+            int i = 0;
+            for (int j = 1; j < list.size(); j++)
+            {
+                IComparable c = (IComparable)list.get(j);
+                if (c.CompareTo(list.get(i)) > 0)
+                    i = j;
+            }
+            return i;
         }
     }
 }
